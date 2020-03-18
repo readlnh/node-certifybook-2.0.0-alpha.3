8df17680f02e72ad79468c2da517343befa60e5f
readlnh <readlnh@163.com>
2020-03-18 18:25:48 +0800

    change runtime version to node-certifybook

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -92,8 +92,8 @@ pub mod opaque {
 
 /// This runtime version.
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("node-template"),
-	impl_name: create_runtime_str!("node-template"),
+	spec_name: create_runtime_str!("node-certifybook"),
+	impl_name: create_runtime_str!("node-certifybook"),
 	authoring_version: 1,
 	spec_version: 1,
 	impl_version: 1,
